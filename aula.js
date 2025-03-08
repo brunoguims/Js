7466af7a5ff254c32bedee2b322b46090ab204fd
Bruno Guimaraes <guimaraesbruno1405@gmail.comgit config listgit config --global user.email guimaraesbruno1405@gmail.com>
2025-03-07 23:57:12 -0300

    first commit

---
+++ aula.js
@@ -0,0 +1,24 @@
+/**Obtenha dados da altura e o gênero (Masculino ou Feminino) de 15 pessoas e apresente os seguintes resultados:
+
+- A maior e a menor altura do grupo;
+- A média de altura das pessoas do gênero Masculino;
+- O número de pessoas do gênero Feminino.
+
+Para o desenvolvimento da atividade o aluno deve utilizar, obrigatoriamente, a linguagem JavaScript.
+
+Após o término da atividade, o aluno deve disponibilizar o link do seu github com a
+resposta.**/
+
+
+
+
+for (var i = 0; i < 3; i++) {
+    var altura = prompt("Digite sua altura: ");
+    al
+    var genero = prompt("Digite seu gênero: ");
+    console.log("Altura: " + altura + ", Gênero: " + genero);
+    
+}
+
+var pesquisa = ({genero,altura})
+console.log(pesquisa)
